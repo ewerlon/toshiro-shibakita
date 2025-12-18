782bdb70c8d8dbfdb9d6b62c36fead7e43a67d73
Ewerlon Silva <33557852+ewerlon@users.noreply.github.com>
2025-12-18 14:56:04 -0400

    Rename 'dados' table to 'clientes' with new schema

--- banco.sql
+++ banco.sql
@@ -1,8 +1,8 @@
-CREATE TABLE dados (
-    AlunoID int,
+CREATE TABLE clientes (
+    ClienteID int,
     Nome varchar(50),
     Sobrenome varchar(50),
     Endereco varchar(150),
     Cidade varchar(50),
-    Host varchar(50)
+    Email varchar(100)
 );
